31d42d021ffed0df4de9fe60f17c3f19cad3b050
thomasdev5832 <eugabrielthome@gmail.com>
2025-07-27 21:12:05 -0300

    refactor: move routes from main to routes

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 mod handlers;
 mod wallet;
 mod provider;
+mod routes;
 
-use axum::{Router, routing::get, Server};
+use axum::{Server};
 use std::net::SocketAddr;
-use handlers::{generate_wallet_handler, get_balance_handler};
 use std::env;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
-    let rpc = env::var("POLYGON_RPC").expect("POLYGON_RPC not set");
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, world!" }))
-        .route("/wallet", get(generate_wallet_handler))
-        .route("/balance/:address", get(get_balance_handler));
+    let _rpc = env::var("POLYGON_RPC").expect("POLYGON_RPC not set");
+
+    let app = routes::create_routes();
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on {}", addr);
+    println!("🦀 Server running at http://{}:{} - press CTRL+C to stop", addr.ip(), addr.port());
+
 
     Server::bind(&addr)
         .serve(app.into_make_service())
--- src/routes.rs
+++ src/routes.rs
@@ -3,6 +3,6 @@ use crate::handlers::{generate_wallet_handler, get_balance_handler};
 
 pub fn create_routes() -> Router {
     Router::new()
-        .route("/generate-wallet", get(generate_wallet_handler))
-        .route("/get-balance/:addr", get(get_balance_handler))
+        .route("/create-wallet", get(generate_wallet_handler))
+        .route("/balance/:address", get(get_balance_handler))
 }
