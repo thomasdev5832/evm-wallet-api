5cced7134f1d17e40a0a5c327033f8bb826a45d4
thomasdev5832 <eugabrielthome@gmail.com>
2025-07-28 22:25:04 -0300

    refactor: switch /create-wallet to post

--- src/routes.rs
+++ src/routes.rs
@@ -1,8 +1,8 @@
-use axum::{Router, routing::get};
+use axum::{Router, routing::{get, post}};
 use crate::handlers::{generate_wallet_handler, get_balance_handler};
 
 pub fn create_routes() -> Router {
     Router::new()
-        .route("/create-wallet", get(generate_wallet_handler))
+        .route("/create-wallet", post(generate_wallet_handler))
         .route("/balance/:address", get(get_balance_handler))
 }
