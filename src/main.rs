ae11602e735232166a7f5610159b952fc92d35b2
thomasdev5832 <eugabrielthome@gmail.com>
2025-08-01 22:37:31 -0300

    chore: include CORS permissions

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod routes;
 use axum::{Server};
 use std::net::SocketAddr;
 use std::env;
+use tower_http::cors::{CorsLayer, Any};
 
 #[tokio::main]
 async fn main() {
@@ -13,13 +14,18 @@ async fn main() {
     let _rpc = env::var("RPC_URL").expect("RPC_URL not set");
 
     let app = routes::create_routes();
+    let cors = CorsLayer::new()
+        .allow_origin(Any) 
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    let app_with_cors = app.layer(cors);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("🦀 Server running at http://{}:{} - press CTRL+C to stop", addr.ip(), addr.port());
 
-
     Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(app_with_cors.into_make_service())
         .await
         .unwrap();
 }
